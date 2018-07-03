fc02fed1e0af7ef46c6691064191ea6f6b1fa9ed
Egor <egor.krushinov@starcounter.com>
2018-07-03 14:20:21 +0300

    Pages now are storing in the Session.Store.

--- LongRunningTransactionApp/Program.cs
+++ LongRunningTransactionApp/Program.cs
@@ -36,9 +36,19 @@ namespace LongRunningTransactionApp
                 return Db.Scope(() =>
                 {
                     Session.Ensure();
+                    
                     var person = Db.SQL<Person>("SELECT p FROM Person p")
                         .FirstOrDefault();
-                    return new PersonJson { Data = person };
+                    
+                    var personJson = Session.Current.Store["PersonJson"] as PersonJson;
+                    if (personJson == null)
+                    {
+                        personJson = new PersonJson {Data = person};
+                        Session.Current.Store["PersonJson"] = personJson;
+                        return personJson;
+                    }
+
+                    return personJson;
                 });
             });
 
@@ -47,9 +57,19 @@ namespace LongRunningTransactionApp
                 return Db.Scope(() =>
                 {
                     Session.Ensure();
+                    
                     var person = Db.SQL<Person>("SELECT p FROM Person p")
                         .FirstOrDefault();
-                    return new Page2() { Data = person };
+                    
+                    var page2 = Session.Current.Store["Page2"] as Page2;
+                    if (page2 == null)
+                    {
+                        page2 = new Page2 { Data = person };
+                        Session.Current.Store["Page2"] = page2;
+                        return page2;
+                    }
+
+                    return page2; ;
                 });
             });
         }
